35a3da2605018d0ae7c997ed53b279e448f56798
João Francisco <joao.rodrigues@viasoft.com.br>
2023-04-13 15:26:20 -0300

    feat: tela para o cart

---
+++ src/pages/MyCart/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export default function Main() {
+  return(
+    <Container>
+      hi mom
+    </Container>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
-import MainPage from './pages/Main'
-import BurgersPage from './pages/Main/Burgers'
-import PizzasPage from './pages/Main/Pizzas'
-import DrinksPage from './pages/Main/Drinks'
+import MainPage from './pages/Main';
+import BurgersPage from './pages/Main/Burgers';
+import PizzasPage from './pages/Main/Pizzas';
+import DrinksPage from './pages/Main/Drinks';
+
+import CartPage from './pages/MyCart';
 
 export function AppRoutes() {
   return (
@@ -12,6 +14,8 @@ export function AppRoutes() {
         <Route path='/burgers' element={<BurgersPage />}/>
         <Route path='/pizzas' element={<PizzasPage />}/>
         <Route path='/drinks' element={<DrinksPage />}/>
+      </Route>
+      <Route path='/cart' element={<CartPage />}>
       </Route>
     </Routes>
   )
